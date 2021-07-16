6981c87cf7acc08737b0af7ad3e55be47e433e53
azaza12345 <61620351+azaza12345@users.noreply.github.com>
2021-07-16 14:44:46 +0300

    Add destroying block effect

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,10 +6,14 @@ using UnityEngine.Events;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _destroyEffect;
     public event UnityAction<Block> BulletHit;
     public void Break()
     {
         BulletHit?.Invoke(this);
+        ParticleSystemRenderer renderer = Instantiate(_destroyEffect, transform.position, Quaternion.identity)
+            .GetComponent<ParticleSystemRenderer>();
+        renderer.material.color = GetComponent<MeshRenderer>().material.color;
         Destroy(gameObject);
     }
 }
